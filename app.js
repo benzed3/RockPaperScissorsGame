14fa5ea125bfd13c4f68f01c3fa7fe285e27be71
benzed3 <benzed3@gmail.com>
2020-12-23 20:47:03 -0500

    added code to just display the winning image

--- app.js
+++ app.js
@@ -12,11 +12,15 @@ $(document).ready(function () {
         console.log(choice);
         console.log(compChoice);
 
-        if (parseInt(compChoice) === parseInt(choice)) {
+        if (parseInt(compChoice) === 1 && parseInt(choice) === 1) {
             $("#resultText").html("<h3>Tied!!</h3>");
+            $("#img1").css("opacity", 1);
+            $("#img2").css("opacity", 0);
+            $("#img3").css("opacity", 0);
+
         }
         else if (parseInt(choice) === 1 && parseInt(compChoice) === 2) {
-            $("#resultText").html("<h3 style='color:red;'>You Lose!!</h3>");
+            $("#resultText").html("<h3 style='color:red;'>Nope!! It was paper!</h3>");
             $("#img1").css("opacity", 0);
             $("#img2").css("opacity", 1);
             $("#img3").css("opacity", 0);
@@ -25,22 +29,29 @@ $(document).ready(function () {
         }
         else if (parseInt(choice) === 1 && parseInt(compChoice) === 3) {
             $("#resultText").html("<h3 style='color:green;'>You Win!!</h3>");
-            $("#img1").css("opacity", 0);
-            $("#img2").css("opacity", 0);
-            $("#img3").css("opacity", 1);
-            winCount++;
-            $("#win").html(winCount);
-        }
-        else if (parseInt(choice) === 2 && parseInt(compChoice) === 1) {
-            $("#resultText").html("<h3 style='color:green;'>You Win!!</h3>");
             $("#img1").css("opacity", 1);
             $("#img2").css("opacity", 0);
             $("#img3").css("opacity", 0);
             winCount++;
             $("#win").html(winCount);
         }
+        else if (parseInt(choice) === 2 && parseInt(compChoice) === 1) {
+            $("#resultText").html("<h3 style='color:green;'>You Win!!</h3>");
+            $("#img1").css("opacity", 0);
+            $("#img2").css("opacity", 1);
+            $("#img3").css("opacity", 0);
+            winCount++;
+            $("#win").html(winCount);
+        }
+        else if (parseInt(compChoice) === 2 && parseInt(choice) === 2) {
+            $("#resultText").html("<h3>Tied!!</h3>");
+            $("#img1").css("opacity", 0);
+            $("#img2").css("opacity", 1);
+            $("#img3").css("opacity", 0);
+
+        }
         else if (parseInt(choice) === 2 && parseInt(compChoice) === 3) {
-            $("#resultText").html("<h3 style='color:red;'>You Lose!!</h3>");
+            $("#resultText").html("<h3 style='color:red;'>Nope!! It was scissors!</h3>");
             $("#img1").css("opacity", 0);
             $("#img2").css("opacity", 0);
             $("#img3").css("opacity", 1);
@@ -48,7 +59,7 @@ $(document).ready(function () {
             $("#lose").html(loseCount);
         }
         else if (parseInt(choice) === 3 && parseInt(compChoice) === 1) {
-            $("#resultText").html("<h3 style='color:red;'>You Lose!!</h3>");
+            $("#resultText").html("<h3 style='color:red;'>Nope!! It was rock!</h3>");
             $("#img1").css("opacity", 1);
             $("#img2").css("opacity", 0);
             $("#img3").css("opacity", 0);
@@ -58,10 +69,17 @@ $(document).ready(function () {
         else if (parseInt(choice) === 3 && parseInt(compChoice) === 2) {
             $("#resultText").html("<h3 style='color:green;'>You Win!!</h3>");
             $("#img1").css("opacity", 0);
-            $("#img2").css("opacity", 1);
-            $("#img3").css("opacity", 0);
+            $("#img2").css("opacity", 0);
+            $("#img3").css("opacity", 1);
             winCount++;
             $("#win").html(winCount);
+        }
+        else if (parseInt(compChoice) === 3 && parseInt(choice) === 3) {
+            $("#resultText").html("<h3>Tied!!</h3>");
+            $("#img1").css("opacity", 0);
+            $("#img2").css("opacity", 0);
+            $("#img3").css("opacity", 1);
+
         }
 
     });
