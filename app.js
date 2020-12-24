f2e6a0659900c4e4b31aa6f965aa7fd5c3c6b17e
benzed3 <benzed3@gmail.com>
2020-12-23 20:14:23 -0500

    added opacity to clear the wrong pictures when choice is made

--- app.js
+++ app.js
@@ -17,31 +17,49 @@ $(document).ready(function () {
         }
         else if (parseInt(choice) === 1 && parseInt(compChoice) === 2) {
             $("#resultText").html("<h3 style='color:red;'>You Lose!!</h3>");
+            $("#img1").css("opacity", 0);
+            $("#img2").css("opacity", 1);
+            $("#img3").css("opacity", 0);
             loseCount++;
             $("#lose").html(loseCount);
         }
         else if (parseInt(choice) === 1 && parseInt(compChoice) === 3) {
             $("#resultText").html("<h3 style='color:green;'>You Win!!</h3>");
+            $("#img1").css("opacity", 0);
+            $("#img2").css("opacity", 0);
+            $("#img3").css("opacity", 1);
             winCount++;
             $("#win").html(winCount);
         }
         else if (parseInt(choice) === 2 && parseInt(compChoice) === 1) {
             $("#resultText").html("<h3 style='color:green;'>You Win!!</h3>");
+            $("#img1").css("opacity", 1);
+            $("#img2").css("opacity", 0);
+            $("#img3").css("opacity", 0);
             winCount++;
             $("#win").html(winCount);
         }
         else if (parseInt(choice) === 2 && parseInt(compChoice) === 3) {
             $("#resultText").html("<h3 style='color:red;'>You Lose!!</h3>");
+            $("#img1").css("opacity", 0);
+            $("#img2").css("opacity", 0);
+            $("#img3").css("opacity", 1);
             loseCount++;
             $("#lose").html(loseCount);
         }
         else if (parseInt(choice) === 3 && parseInt(compChoice) === 1) {
             $("#resultText").html("<h3 style='color:red;'>You Lose!!</h3>");
+            $("#img1").css("opacity", 1);
+            $("#img2").css("opacity", 0);
+            $("#img3").css("opacity", 0);
             loseCount++;
             $("#lose").html(loseCount);
         }
         else if (parseInt(choice) === 3 && parseInt(compChoice) === 2) {
             $("#resultText").html("<h3 style='color:green;'>You Win!!</h3>");
+            $("#img1").css("opacity", 0);
+            $("#img2").css("opacity", 1);
+            $("#img3").css("opacity", 0);
             winCount++;
             $("#win").html(winCount);
         }
