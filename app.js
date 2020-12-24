597af344a184a860c37bf5fa5584a762b8979e70
benzed3 <benzed3@gmail.com>
2020-12-23 19:54:10 -0500

    added color to win/lose text

--- app.js
+++ app.js
@@ -16,33 +16,32 @@ $(document).ready(function () {
             $("#resultText").html("<h3>Tied!!</h3>");
         }
         else if (parseInt(choice) === 1 && parseInt(compChoice) === 2) {
-            $("#resultText").html("<h3>You Lose!!</h3>");
+            $("#resultText").html("<h3 style='color:red;'>You Lose!!</h3>");
             loseCount++;
-            $(".imgDiv2").addClass("border");
             $("#lose").html(loseCount);
         }
         else if (parseInt(choice) === 1 && parseInt(compChoice) === 3) {
-            $("#resultText").html("<h3>You Win!!</h3>");
+            $("#resultText").html("<h3 style='color:green;'>You Win!!</h3>");
             winCount++;
             $("#win").html(winCount);
         }
         else if (parseInt(choice) === 2 && parseInt(compChoice) === 1) {
-            $("#resultText").html("<h3>You Win!!</h3>");
+            $("#resultText").html("<h3 style='color:green;'>You Win!!</h3>");
             winCount++;
             $("#win").html(winCount);
         }
         else if (parseInt(choice) === 2 && parseInt(compChoice) === 3) {
-            $("#resultText").html("<h3>You Lose!!</h3>");
+            $("#resultText").html("<h3 style='color:red;'>You Lose!!</h3>");
             loseCount++;
             $("#lose").html(loseCount);
         }
         else if (parseInt(choice) === 3 && parseInt(compChoice) === 1) {
-            $("#resultText").html("<h3>You Lose!!</h3>");
+            $("#resultText").html("<h3 style='color:red;'>You Lose!!</h3>");
             loseCount++;
             $("#lose").html(loseCount);
         }
         else if (parseInt(choice) === 3 && parseInt(compChoice) === 2) {
-            $("#resultText").html("<h3>You Win!!</h3>");
+            $("#resultText").html("<h3 style='color:green;'>You Win!!</h3>");
             winCount++;
             $("#win").html(winCount);
         }
